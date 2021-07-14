16fc347a9b0f3a4397804f3c19b53cd9c4039729
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-14 16:10:47 +0300

    Add all the cases for the reducer named manageFriends and pass all the tests.

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,20 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = {friends: []}, action) {
+  switch (action.type) {
+    case "friends/add":
+      return {
+        friends: [...state.friends,
+          {
+            name: action.payload.name,
+            hometown: action.payload.hometown,
+            id: action.payload.id
+          }
+        ]
+      }
+    case "friends/remove": 
+    return {
+      friends: state.friends.filter(friend => friend.id !== action.payload)
+    }
+    default:
+      return state;
+  }
 }
